ad94a9fc466ffe26345e92f742fd4df65e096453
msh2481 <msh2481@ya.ru>
2022-01-08 11:37:12 +0300

    1641631032

---
+++ utils.py
@@ -0,0 +1,10 @@
+from time import time
+
+def git_save():
+    from os import system as cmd
+    print(cmd('git switch autosave'))
+    print(cmd('git add .'))
+    print(cmd('git commit -m' + str(int(time()))))
+    print(cmd('git push'))
+
+log = open('log.txt', 'w')
