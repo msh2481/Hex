8c85660ebfb8fe1a1d0b56caba1fea1d66408dd0
msh2481 <msh2481@ya.ru>
2022-01-08 11:38:01 +0300

    1641631081

--- routines.py
+++ routines.py
@@ -1,5 +1,10 @@
-def contest(first, second):
-    board = Board(n, m)
+from board import Board
+from copy import deepcopy
+
+def contest(size, first, second):
+    board = Board(size, size)
+    first.clear_history()
+    second.clear_history()
     while not board.winner():
         player = first if board.player == 1 else second
         temp = player.make_move(board)
@@ -10,20 +15,20 @@ def contest(first, second):
             board.rollback()
     return board.winner()
 
-def tournament(first, second, n = 1000):
+def tournament(size, first, second, n = 1000):
     w = 0
     for game in range(n):
         if game % 2 == 0:
-            w += contest(first, second) == 1
+            w += contest(size, first, second) == 1
         else:
-            w += contest(second, first) == -1
+            w += contest(size, second, first) == -1
     return w / n
 
-def training_camp(model, n = 10):
+def training_camp(size, model, n = 10):
     old = deepcopy(model)
     model.learning = True
     for game in range(n):
-        contest(model, model)
+        contest(size, model, model)
     new = deepcopy(model)
     old.learning = False
     new.learning = False
