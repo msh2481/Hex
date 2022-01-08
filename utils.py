de0ef2a6a9284d2cb07b9fc06151af887764bcec
msh2481 <msh2481@ya.ru>
2022-01-08 11:48:01 +0300

    1641631681

--- utils.py
+++ utils.py
@@ -3,12 +3,9 @@ from time import time
 log = open('log.txt', 'w')
 
 def git_save():
-    global log
     from os import system as cmd
-    log.close()
-    cmd('git switch autosave >log.txt')
-    cmd('git add . >log.txt'),
-    cmd('git commit -m' + str(int(time())) + '>log.txt')
-    cmd('git push >log.txt')
-    log = open('log.txt', 'a')
+    cmd('git switch autosave >git.txt')
+    cmd('git add . >git.txt'),
+    cmd('git commit -m' + str(int(time())) + '>git.txt')
+    cmd('git push >git.txt')
 
